cd27a78cc1afaa70ace8b49b677ceaca02518d89
brunocamps <bruno.mcamps@outlook.com>
2022-05-25 07:34:04 -0300

    Added validation to signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,22 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    #debugger
   end
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) #not the final impl
+    if @user.save
+      #handle successful save
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
