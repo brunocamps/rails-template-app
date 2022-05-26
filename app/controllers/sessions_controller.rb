37c8192620b7515ece435ec796f1edccc21c7116
brunocamps <bruno.mcamps@outlook.com>
2022-05-26 12:59:50 -0300

    Updated sessions_controller and corrected routes.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #log in the user and redirect to the user's show page
+    else
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
 end
