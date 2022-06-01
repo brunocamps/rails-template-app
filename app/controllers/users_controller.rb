989fcc346396dc83f0eb695e8465dc2506ad8d47
brunocamps <bruno.mcamps@outlook.com>
2022-06-01 07:00:54 -0300

    Test failing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Handle a successful update.
+    end
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
