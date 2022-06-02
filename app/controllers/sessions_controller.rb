93a0e2f887ca9ad7217e335b869e1a0949c557cd
brunocamps <bruno.mcamps@outlook.com>
2022-06-02 10:45:00 -0300

    Implemented friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      reset_session
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      #reset_session
       log_in user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
+      # log_in user
+      # redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
