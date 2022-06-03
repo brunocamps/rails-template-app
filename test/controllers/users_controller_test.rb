57cd7f6df09b162a35c809a636bc72cbe3248af5
brunocamps <bruno.mcamps@outlook.com>
2022-06-02 21:34:18 -0300

    Created working user index page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -33,6 +33,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   # test "should get new" do
   #   get signup_path
   #   assert_response :success
