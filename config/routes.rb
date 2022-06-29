de7c1d482eae318c9d558816b8a672bbd5b75268
brunocamps <bruno.mcamps@outlook.com>
2022-06-29 20:34:06 -0300

    Following and unfollowing working

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,18 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
   # get    '/edit',    to: 'users#edit'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+
+
   resources :account_activations, only: [:edit]
   # resources: passwords reset?
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
